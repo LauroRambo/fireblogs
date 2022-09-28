cdc896a5eab7ec0d5ccfe844a27d5337148f535d
LauroRambo <51201434+LauroRambo@users.noreply.github.com>
2022-09-28 16:54:19 -0300

    edit firebase data

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="blog-card">
     <div v-show="editPost" class="icons">
-      <div class="icon">
+      <div @click="editBlog" class="icon">
         <Edit class="edit" />
       </div>
       <div @click="deletePost" class="icon">
@@ -33,7 +33,10 @@ export default {
   methods: {
     deletePost() {
       this.$store.dispatch("deletePost", this.post.blogID);
-    }
+    },
+    editBlog() {
+      this.$router.push({ name: 'EditBlog', params: { blogid: this.post.blogID } });
+    },
   },
   computed: {
     editPost() {
--- src/store/index.js
+++ src/store/index.js
@@ -52,6 +52,12 @@ export default new Vuex.Store({
     toggleEditPost(state, payload){
       state.editPost = payload;
     },
+    setBlogState(state, payload) {
+      state.blogTitle = payload.blogTitle;
+      state.blogHTML = payload.blogHTML;
+      state.blogPhotoFileURL = payload.blogPhotoFileURL;
+      state.blogPhotoName = payload.blogCoverPhoto;
+    },
     filterBlogPost(state, payload) {
       state.blogPosts = state.blogPosts.filter((post) => post.blogID !== payload);
     },
@@ -105,11 +111,16 @@ export default new Vuex.Store({
             blogCoverPhoto: doc.data().blogCoverPhoto,
             blogTitle: doc.data().blogTitle,
             blogDate: doc.data().date,
+            blogCoverPhotoName: doc.data().blogCoverPhotoName,
           };
           state.blogPosts.push(data);
         }
       });
       state.postLoaded = true;
+    },
+    async updatePost({commit, dispatch}, payload) {
+      commit("filterBlogPost", payload);
+      await dispatch("getPost");
     },
     async deletePost({ commit }, payload) {
       const getPost = await db.collection('blogPosts').doc(payload);
